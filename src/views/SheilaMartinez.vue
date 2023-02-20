45d3401541a1bbd1238b661791b06994bdef2f0a
Sheila <sheila.martinezdiez@gmail.com>
2023-02-20 13:35:34 +0100

    a

--- src/views/SheilaMartinez.vue
+++ src/views/SheilaMartinez.vue
@@ -1,1 +1,18 @@
-# hej!
+<script>
+  export default {
+    data() {
+      return {
+        test: 'test'
+      }
+    }
+  }
+</script>
+
+<template>
+  <b-card>
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+  </b-card>
+</template>
