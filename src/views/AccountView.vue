d98dc69778621f06913786a9202beac2e825f09d
Diyana Rahim <diyana.96@hotmail.com>
2023-02-21 14:04:06 +0100

    hej

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,12 +1,15 @@
 <script>
   import AccountComp from '../components/AccountComponents/AccountComp.vue'
+  // import BetalningComponent from '../components/BetalningComponent.vue'
 
   export default {
     components: { AccountComp }
+    // components: { AccountComp, BetalningComponent }
   }
 </script>
 
 <template>
   <!-- AccountComp temporary main nav -->
   <AccountComp />
+  <!-- <BetalningComponent /> -->
 </template>
