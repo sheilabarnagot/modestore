6fdb73266e1131db397185247609ad4b7bdac3ba
Diyana Rahim <diyana.96@hotmail.com>
2023-02-22 13:08:41 +0100

    payment

--- src/components/BetalningComponent.vue
+++ src/components/BetalningComponent.vue
@@ -15,8 +15,8 @@
     <input type="radio" id="form" value="Swish" v-model="payment" />
     <label for="swish">Swish</label>
 
-    <input type="radio" id="form" value="Klarna" v-model="payment" />
-    <label for="klarna">Klarna</label>
+    <input type="radio" id="form" value="faktura" v-model="payment" />
+    <label for="faktura">faktura</label>
   </div>
 </template>
 
@@ -30,8 +30,8 @@
   }
 </script>
 
-<style lang="scss">
-  // input {
-  //   width: 50vw;
-  // }
+<style scoped>
+  /* input #form label {
+    position: absolute;
+  } */
 </style>
