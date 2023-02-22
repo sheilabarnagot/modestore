51761348f34a79f41bd7cd6a510f27519fe31868
Lilas Alnesr <lelas.alneser@gmail.com>
2023-02-22 15:46:38 +0100

    test

--- src/components/BetalningComponent.vue
+++ src/components/BetalningComponent.vue
@@ -1,20 +1,56 @@
 <template>
+  <!-- <div>
+    <div>
+      <input
+        type="radio"
+        class="form-check-radio"
+        id="a"
+        name="bankinfo"
+        value="cardnumber"
+      />
+      <label class="form-check-radio" for="a">A</label>
+    </div>
+    <div>
+      <input
+        type="radio"
+        class="form-check-radio"
+        id="b"
+        name="bankinfo"
+        value="cvc"
+      />
+      <label class="form-check-radio" for="b">B</label>
+    </div>
+    <div>
+      <input
+        type="radio"
+        class="form-check-radio"
+        id="c"
+        name="bankinfo"
+        value="my"
+      />
+      <label class="form-check-radio" for="c">C</label>
+    </div>
+  </div> -->
+
   <div>
     <h4>Payment: {{ payment }}</h4>
   </div>
-  <!-- <div class="container"> -->
-  <label style="display: block" for="kort">Credit Card</label>
-  <input type="radio" id="form" value="Kort" v-model="payment" />
 
-  <input
-    style="display: none"
-    type="text"
-    :value="creditnumber"
-    placeholder="Credit Number"
-  />
+  <div>
+    <input type="radio" id="form" value="Kort" v-model="payment" />
+    <label style="display: block" for="kort">Credit Card</label>
 
-  <input style="display: none" type="text" :value="my" placeholder="M/Y" />
-  <input style="display: none" type="text" :value="cvc" placeholder="cvc" />
+    <input
+      style="display: none"
+      type="text"
+      :value="creditnumber"
+      placeholder="CreditNumber"
+    />
+
+    <input style="display: none" type="text" :value="my" placeholder="M/Y" />
+    <input style="display: none" type="text" :value="cvc" placeholder="cvc" />
+  </div>
+
   <label style="display: block" for="swish">Swish</label>
   <input type="radio" id="form" value="Swish" v-model="payment" />
 
@@ -25,8 +61,14 @@
     placeholder="Skriv ditt nummer"
   />
   <label style="display: block" for="faktura">faktura</label>
-  <input type="radio" id="form" value="faktura" v-model="payment" />
-  <!-- </div> -->
+  <input
+    @click="OnFaktura"
+    type="radio"
+    id="form"
+    value="faktura"
+    v-model="payment"
+  />
+
   <button @click="betala">Bekräfta betalning</button>
 </template>
 
@@ -39,14 +81,9 @@
         cvc: 'CVC',
         creditnumber: 'Credi† Number'
       }
+    },
+    methods: {
+      OnFaktura() {}
     }
   }
 </script>
-
-<!-- <style scoped>
-  input #form label {
-    position: absolute;
-  }
-</style> -->
-
-<style lang="scss"></style>
