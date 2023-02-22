2a54e0a93a2ce797f2a51225b21c39a542dac2b7
Diyana Rahim <diyana.96@hotmail.com>
2023-02-22 14:49:47 +0100

    .

--- src/components/BetalningComponent.vue
+++ src/components/BetalningComponent.vue
@@ -2,35 +2,32 @@
   <div>
     <h4>Payment: {{ payment }}</h4>
   </div>
-  <div class="container">
-    <input type="radio" id="form" value="Kort" v-model="payment" />
+  <!-- <div class="container"> -->
+  <label style="display: block" for="kort">Credit Card</label>
+  <input type="radio" id="form" value="Kort" v-model="payment" />
 
-    <label style="display: block" for="kort">Credit Card</label>
+  <input
+    style="display: none"
+    type="text"
+    :value="creditnumber"
+    placeholder="Credit Number"
+  />
 
-    <input
-      style="display: none"
-      type="text"
-      :value="creditnumber"
-      placeholder="Credit Number"
-    />
+  <input style="display: none" type="text" :value="my" placeholder="M/Y" />
+  <input style="display: none" type="text" :value="cvc" placeholder="cvc" />
+  <label style="display: block" for="swish">Swish</label>
+  <input type="radio" id="form" value="Swish" v-model="payment" />
 
-    <input style="display: none" type="text" :value="my" placeholder="M/Y" />
-    <input style="display: none" type="text" :value="cvc" placeholder="cvc" />
-
-    <input type="radio" id="form" value="Swish" v-model="payment" />
-    <label style="display: block" for="swish">Swish</label>
-    <input
-      style="display: none"
-      type="text"
-      :value="number"
-      placeholder="Skriv ditt nummer"
-    />
-
-    <input type="radio" id="form" value="faktura" v-model="payment" />
-    <label style="display: block" for="faktura">faktura</label>
-
-    <button @click="betala">Bekräfta betalning</button>
-  </div>
+  <input
+    style="display: none"
+    type="text"
+    :value="number"
+    placeholder="Skriv ditt nummer"
+  />
+  <label style="display: block" for="faktura">faktura</label>
+  <input type="radio" id="form" value="faktura" v-model="payment" />
+  <!-- </div> -->
+  <button @click="betala">Bekräfta betalning</button>
 </template>
 
 <script>
