9579665b19996ffb0bddcc3b9efb7e61cb89c9dc
Lilas Alnesr <lelas.alneser@gmail.com>
2023-02-22 13:11:07 +0100

    style

--- src/components/BetalningComponent.vue
+++ src/components/BetalningComponent.vue
@@ -5,12 +5,9 @@
   <div class="container">
     <input type="radio" id="form" value="Kort" v-model="payment" />
     <label for="kort">Credit Card</label>
-
     <input type="text" :value="namn" placeholder="Credit Number" />
-
     <input type="text" :value="namn" placeholder="M/Y" />
-
-    <input type="text" :value="namn" placeholder="CVC" />
+    <input type="text" :value="namn" placeholder="M/Y" />
 
     <input type="radio" id="form" value="Swish" v-model="payment" />
     <label for="swish">Swish</label>
@@ -34,4 +31,7 @@
   // input {
   //   width: 50vw;
   // }
+  div.container {
+    display: flex;
+  }
 </style>
