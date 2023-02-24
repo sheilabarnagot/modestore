a903df1881d016a11a3b10e0302100725a6fa850
ponpon <pontus00valdes@gmail.com>
2023-02-24 13:16:02 +0100

    trying to fix lint error

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
-import VueI18n from "@intlify/unplugin-vue-i18n/vite";
-import path from "node:path";
+import VueI18n from '@intlify/unplugin-vue-i18n/vite'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [
@@ -15,8 +15,6 @@ export default defineConfig({
       include: path.resolve(__dirname, './src/locales/**')
     })
   ]
-
 })
-
 
 // https://github.com/intlify/vite-plugin-vue-i18n/issues/74
