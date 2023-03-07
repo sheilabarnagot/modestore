2a8599af7a994879d1c17e9597ce5ce79645fb0c
Diyana Rahim <diyana.96@hotmail.com>
2023-03-07 12:46:22 +0100

    stripe test

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import { i18n } from './plugins/i18n.js'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+// import { stripe } from './plugins/StripePlugin.js'
 
 createApp(App).use(router).use(store).use(i18n).mount('#app')
