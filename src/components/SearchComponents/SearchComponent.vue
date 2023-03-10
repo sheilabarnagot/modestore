286a1fc1e7de22af67389c7f74117e725a80f315
Lilas Alnesr <lelas.alneser@gmail.com>
2023-03-10 18:03:19 +0100

    ta bort Script från SearchComponent

--- src/components/SearchComponents/SearchComponent.vue
+++ src/components/SearchComponents/SearchComponent.vue
@@ -6,35 +6,35 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        searchTerm: '',
-        products: [],
-        filteredProducts: []
-      }
-    },
-    async mounted() {
-      await this.fetchData()
-    },
-    methods: {
-      async fetchData() {
-        const response = await fetch('../../assets/products.json')
-        const result = await response.json()
-        this.products = result.productpics
-        this.filterProducts()
-      },
-      filterProducts(event) {
-        console.log(event)
+  // export default {
+  //   data() {
+  //     return {
+  //       searchTerm: '',
+  //       products: [],
+  //       filteredProducts: []
+  //     }
+  //   },
+  //   async mounted() {
+  //     await this.fetchData()
+  //   },
+  //   methods: {
+  //     async fetchData() {
+  //       const response = await fetch('../../assets/products.json')
+  //       const result = await response.json()
+  //       this.products = result.productpics
+  //       this.filterProducts()
+  //     },
+  //     filterProducts(event) {
+  //       console.log(event)
 
-        return this.filteredProducts = this.products.filter((entry) =>
-          Object.keys(entry).some((key) =>
-            ('' + entry[key])
-              .toLowerCase()
-              .includes(this.searchTerm.toLowerCase())
-          )
-        )
-      }
-    }
-  }
+  //       return (this.filteredProducts = this.products.filter((entry) =>
+  //         Object.keys(entry).some((key) =>
+  //           ('' + entry[key])
+  //             .toLowerCase()
+  //             .includes(this.searchTerm.toLowerCase())
+  //         )
+  //       ))
+  //     }
+  //   }
+  // }
 </script>
