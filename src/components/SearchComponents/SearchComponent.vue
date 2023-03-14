dac058f362d1699664d50ce853a47429df9941d8
ponpon <pontus00valdes@gmail.com>
2023-03-14 13:05:54 +0100

    add temp styling to individual products

--- src/components/SearchComponents/SearchComponent.vue
+++ src/components/SearchComponents/SearchComponent.vue
@@ -1,3 +1,26 @@
 <template>
-  {{ this.$store.state.searchedItems }}
+  <div>
+    <template v-for="item in searchedItem" :key="item.id">
+      <img :src="item.src" />
+      <p>{{ item.name }}</p>
+      <p>{{ item.product }}</p>
+      <p>{{ item.price }}</p>
+    </template>
+  </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      searchedItem() {
+        return this.$store.state.searchedItems
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  img {
+    max-width: 100%;
+  }
+</style>
